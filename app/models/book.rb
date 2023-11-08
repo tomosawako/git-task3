c7f9e442f74ae961fed3d00b29f666640b5630ac
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-11-08 14:03:45 +0000

    [Update2]booksコントローラーの作成

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+
+  has_one_attached :image
+  belong_to :user
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
   devise_for :users
-root to: "homes#top"
+
+  resources :books, only: [:new, :index, :show]
+
+  get 'homes/about' => "homes#about", as: 'about'
+
 end
