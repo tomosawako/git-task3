e44ed61d0c103bb6f67e66c680bc796772d3c553
tomosawako <145893964+tomosawako@users.noreply.github.com>
2023-11-14 09:02:53 +0000

    [Update]userページの編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
 
   def index
-    @user = User.all
+    @users = User.all
+    @user = User.find(current_user.id)
+    @books = @user.books
   end
 
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
+
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+private
+
+ def user_params
+   params.require(:user).permit(:name, :introduction, :profile_image)
+ end
+
 end
